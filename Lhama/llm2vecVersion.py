8e3b70dfcd83182223bf3fe06504d06131553574
pedroVelosoFernandes <pedrohenrique.veloso.fernandes@gmail.com>
2024-11-18 01:12:47 -0300

    documentos tbm

--- Lhama/llm2vecVersion.py
+++ Lhama/llm2vecVersion.py
@@ -2,7 +2,7 @@ import torch
 from huggingface_hub import login
 from llm2vec.llm2vec import LLM2Vec
 
-login("")
+login("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")
 
 l2v = LLM2Vec.from_pretrained(
     "meta-llama/Meta-Llama-3.1-8B-Instruct",
@@ -23,4 +23,15 @@ queries = [
 
 q_reps = l2v.encode(queries)
 
-print("Query representations generated successfully!")
+documents = [
+    "As a general guideline, the CDC's average requirement of protein for women ages 19 to 70 is 46 grams per day. But, as you can see from this chart, you'll need to increase that if you're expecting or training for a marathon. Check out the chart below to see how much protein you should be eating each day.",
+    "Definition of summit for English Language Learners. : 1  the highest point of a mountain : the top of a mountain. : 2  the highest level. : 3  a meeting or series of meetings between the leaders of two or more governments.",
+]
+d_reps = l2v.encode(documents)
+print(d_reps)
+
+q_reps_norm = torch.nn.functional.normalize(q_reps, p=2, dim=1)
+d_reps_norm = torch.nn.functional.normalize(d_reps, p=2, dim=1)
+cos_sim = torch.mm(q_reps_norm, d_reps_norm.transpose(0, 1))
+
+print(cos_sim)
